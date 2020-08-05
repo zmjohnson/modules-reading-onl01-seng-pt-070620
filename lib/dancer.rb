ad96c4e342d23bf102ca6c0e837ed59e6d8a5e5d
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-05 20:06:06 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,12 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Kid
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
